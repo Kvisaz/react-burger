890f774cb6a4346c3fcca5f1a77f6c4fedf2a0a1
sergeytokarev <stokarev@novakidschool.com>
2021-07-25 17:31:36 +0700

    sprint1/step1 - App columns layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 
 import DATA from './utils/data.json';
 import {AppHeader} from "./components/app-header/app-header";
@@ -7,11 +7,19 @@ import {AppHeader} from "./components/app-header/app-header";
 console.log('DATA', DATA);
 
 function App() {
-  return (
-    <div className="App">
-      <AppHeader />
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <AppHeader/>
+            <div className={styles.row}>
+                <div className={styles.col_left}>
+                    <span className='text text_type_main-large'>Соберите бургер</span>
+                </div>
+                <div className={styles.col_right}>
+                    dd
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
