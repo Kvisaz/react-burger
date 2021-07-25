0e044774a4375d10e007f8a7ff0179e084da3f6d
sergeytokarev <stokarev@novakidschool.com>
2021-07-25 19:29:36 +0700

    sprint1/step1 - BurgerConstructorSection

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './burger-constructor.module.css';
 import {BurgerConstructorTabs} from "./components/burger-constructor-tabs/burger-constructor-tabs";
+import {BurgerConstructorSection} from "./components/burger-constructor-section/burger-constructor-section";
 
 interface IBurgerConstructorProps {
 
@@ -10,6 +11,15 @@ export function BurgerConstructor({}: IBurgerConstructorProps) {
     return (
         <div className='mt-5'>
             <BurgerConstructorTabs/>
+            <BurgerConstructorSection title={'Булки'}>
+                ddd
+            </BurgerConstructorSection>
+            <BurgerConstructorSection title={'Соусы'}>
+                ddd
+            </BurgerConstructorSection>
+            <BurgerConstructorSection title={'Начинки'}>
+                ddd
+            </BurgerConstructorSection>
         </div>
     );
 }
---
+++ src/components/burger-constructor/components/burger-constructor-section/burger-constructor-section.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './burger-constructor-section.module.css';
+
+export interface IBurgerConstructorSectionProps {
+    title: string;
+    children: React.ReactNode;
+}
+
+export function BurgerConstructorSection({title, children}: IBurgerConstructorSectionProps) {
+    return (
+        <div className={`${styles.section} mt-10`}>
+            <span className='text text_type_main-medium'>{title}</span>
+            <div className='mt-6'>
+                {children}
+            </div>
+        </div>
+    );
+}
