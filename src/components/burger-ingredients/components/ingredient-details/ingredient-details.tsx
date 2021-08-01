745698db3dbaeb089ce709d91b5e65b152f210d5
sergeytokarev <stokarev@novakidschool.com>
2021-08-01 15:10:46 +0700

    sprint1/step2 - propTypes fix for IngredientDetails

--- src/components/burger-ingredients/components/ingredient-details/ingredient-details.tsx
+++ src/components/burger-ingredients/components/ingredient-details/ingredient-details.tsx
@@ -17,10 +17,10 @@ interface IDetailsProps extends IBurgerPart {
 IngredientDetails.propTypes = {
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    calories: PropTypes.string.isRequired,
-    proteins: PropTypes.string.isRequired,
-    fat: PropTypes.string.isRequired,
-    carbohydrates: PropTypes.string.isRequired,
+    calories: PropTypes.number.isRequired,
+    proteins: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired,
 };
 
 export function IngredientDetails({image, name, carbohydrates, fat, calories, proteins}: IDetailsProps) {
