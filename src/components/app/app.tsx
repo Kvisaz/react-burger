b5ea1a8c119b7e5e39eab2011bebc787d472a3cd
Sergey Tokarev <sergey.zoomaster@gmail.com>
2021-08-11 04:43:35 +0700

    sprint2/step1 - app cebab case

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useReducer } from 'react';
-import styles from './App.module.css';
-import { AppHeader } from './components/app-header/app-header';
-import { BurgerIngredients } from './components/burger-ingredients/burger-ingredients';
-import { BurgerConstructor } from './components/burger-constructor/burger-constructor';
-import { Api } from './service/Api';
-import { Modal } from './components/common/modal/modal';
-import { IngredientDetails } from './components/burger-ingredients/components/ingredient-details/ingredient-details';
-import { OrderDetails } from './components/burger-constructor/components/order-details/order-details';
-import { AppContext } from './service/AppContext';
-import { reducer } from './service/reducer';
-import { IBurgerActionType } from './model/IBurgerAction';
+import styles from './app.module.css';
+import { AppHeader } from '../app-header/app-header';
+import { BurgerIngredients } from '../burger-ingredients/burger-ingredients';
+import { BurgerConstructor } from '../burger-constructor/burger-constructor';
+import { Api } from '../../service/Api';
+import { Modal } from '../common/modal/modal';
+import { IngredientDetails } from '../burger-ingredients/components/ingredient-details/ingredient-details';
+import { OrderDetails } from '../burger-constructor/components/order-details/order-details';
+import { AppContext } from '../../service/AppContext';
+import { reducer } from '../../service/reducer';
+import { IBurgerActionType } from '../../model/IBurgerAction';
 
 const API_DATA_END_POINT = 'https://norma.nomoreparties.space/api/ingredients';
 const API_ORDER_END_POINT = 'https://norma.nomoreparties.space/api/orders';
