ae54e86c92d263254204967cbdd2ce63eb79029e
sergeytokarev <stokarev@novakidschool.com>
2021-09-05 17:34:24 +0700

    sprint3/step2 - reset-password layout

--- src/pages/reset-password/reset-password.tsx
+++ src/pages/reset-password/reset-password.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import styles from './reset-password.module.css';
+import {Button, Input, PasswordInput} from '@ya.praktikum/react-developer-burger-ui-components';
+import {Link} from 'react-router-dom';
+import {Routes} from '../../services/Routes';
 
 export function ResetPassword() {
-    return (<div>
-        ResetPassword
+
+    const onPasswordChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log('onPasswordChange ', e.target.value);
+    }, [])
+
+    const onCodeChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log('onCodeChange ', e.target.value);
+    }, [])
+
+
+    const onButtonClick = useCallback(() => {
+        console.log('onButtonClick');
+    }, [])
+
+    return (<div className={styles.wrap}>
+        <div className={styles.content}>
+            <div className={`text text_type_main-medium ${styles.label}`}>Восстановление пароля</div>
+            <PasswordInput name={'password'} value={''} onChange={onPasswordChange}/>
+            <Input placeholder={'Введите код из письма'} value={''} onChange={onCodeChange}/>
+            <Button onClick={onButtonClick} size={'medium'} type={'primary'}>Сохранить</Button>
+            <div className={`text text_type_main-small ${styles.about}`}>
+                <div>Вспомнили пароль? <Link to={Routes.login}>Войти</Link></div>
+            </div>
+        </div>
     </div>)
 }
