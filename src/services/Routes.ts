4741282f08cf06dc3bc6f4bd426634228fbf5ee6
sergeytokarev <stokarev@novakidschool.com>
2021-09-27 18:57:39 +0700

    sprint-4/step-1 - Routes and prettier

--- src/services/Routes.ts
+++ src/services/Routes.ts
@@ -1,20 +1,23 @@
 export const Routes = {
-    main: '/',
-    login: '/login',
-    register: '/register',
-    forgotPassword: '/forgot-password',
-    resetPassword: '/reset-password',
-    profile: '/profile',
-    orders: '/profile/orders',
-    orderPage: '/order/:id',
-    ingredient: '/ingredients/:id',
-    ingredientLinkCreator: (id: string) => `/ingredients/${id}`,
-    orderPageLinkCreator: (id: number) => `/order/${id}`
-}
+  main: '/',
+  login: '/login',
+  register: '/register',
+  forgotPassword: '/forgot-password',
+  resetPassword: '/reset-password',
+  profile: '/profile',
+  orders: '/profile/orders',
+  orderPage: '/profile/orders/:id',
+  ingredient: '/ingredients/:id',
+  feed: './feed',
+  feedId: './feed/:id',
+  ingredientLinkCreator: (id: string) => `/ingredients/${id}`,
+  orderPageLinkCreator: (id: number) => `/order/${id}`,
+  feedIdLinkCreator: (id: number) => `/feed/${id}`,
+};
 
 
 export interface LocationState {
-    modalIngredient?: boolean;
-    modalOrder?: boolean;
-    backTo: string;
+  modalIngredient?: boolean;
+  modalOrder?: boolean;
+  backTo: string;
 }
