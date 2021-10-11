15f87e3228f0fe9c9d14f30da3f0f0a677404a58
Sergey Tokarev <sergey.zoomaster@gmail.com>
2021-10-11 21:00:46 +0700

    sprint-4/step-1 - logo link

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { AppHeaderItem, ICON } from './components/app-header-item/app-header-item';
 import styles from './app-header.module.css';
 import { Routes } from '../../services/Routes';
+import { Link } from 'react-router-dom';
 
 export function AppHeader() {
   const className = `${styles.header} mt-4 mb-4`;
@@ -14,7 +15,7 @@ export function AppHeader() {
                 <AppHeaderItem text='Лента заказов' icon={ICON.List} to={Routes.orderFeed} />
             </span>
         <span className={styles.center}>
-                <Logo />
+          <Link className={styles.link} to={Routes.main}><Logo /></Link>
             </span>
         <span className={styles.right}>
                 <AppHeaderItem text='Личный кабинет' icon={ICON.Person} to={Routes.profile} />
