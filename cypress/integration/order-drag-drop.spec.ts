de0dfefc6b3221ae0be8eb73c5f21fe4cbc16e54
Sergey Tokarev <sergey.zoomaster@gmail.com>
2021-10-20 06:07:46 +0700

    sprint-4/step-3 - cypress should basket order visible

--- cypress/integration/order-drag-drop.spec.ts
+++ cypress/integration/order-drag-drop.spec.ts
@@ -3,49 +3,58 @@ describe('drag and drop burger parts', function() {
     cy.visit('http://localhost:3000');
   });
 
+  it('should basket order visible', () => {
+    waitOrderBasket();
+  });
+  
 
   it('should drag first item to the order', () => {
+    waitOrderBasket();
     dragDropByIndex(0);
   });
 
   it('should drag some items to the basket', () => {
+    waitOrderBasket();
     dragDropByIndex(2);
     dragDropByIndex(4);
     dragDropByIndex(6);
     getOrderButton().then($button => {
-      if ($button.is(':visible')){
-        console.log('order button is visible')
+      if ($button.is(':visible')) {
+        console.log('order button is visible');
       }
-    })
-  })
+    });
+  });
 
 });
 
-
-function getBurgerParts(){
+function getBurgerParts() {
   return cy.get('[data-cy=burger-part]');
 }
 
-function getOrderBasket(){
+function getOrderBasket() {
   return cy.get('[data-cy=burger-basket]');
 }
 
-function getOrderButton(){
+function waitOrderBasket() {
+  return cy.get('[data-cy=burger-basket]', {timeout: 10000}).should('be.visible');
+}
+
+function getOrderButton() {
   return cy.get('[data-cy=burger-basket-order-button]');
 }
 
-function getOrderedItems(){
+function getOrderedItems() {
   return cy.get('[data-cy=burger-basket-order-item]');
 }
 
-function getDraggableOrderedItems(){
+function getDraggableOrderedItems() {
   return cy.get('[data-cy=burger-basket-order-item]').filter('[draggable]');
 }
 
 function dragDropByIndex(index: number) {
   const dataTransfer = new DataTransfer();
 
-   getBurgerParts().eq(index).trigger('dragstart', {
+  getBurgerParts().eq(index).trigger('dragstart', {
     dataTransfer,
   });
 
